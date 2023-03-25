92e59528ccbe450086d3901a0b849fab071dd0c5
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-03-25 19:06:24 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
-
+import phonenumbers
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
