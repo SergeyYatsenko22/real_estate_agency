559ed5599f43aa230cd24913de6ef4ad4a6fca06
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-03-25 20:52:17 +0300

    Added model Owner

--- property/admin.py
+++ property/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
+from .models import Pokemon, PokemonEntity
 
-from .models import Flat, Complaint
-
-
-
-class FlatAdmin(admin.ModelAdmin):
-    search_fields = ["town", "address"]
-    readonly_fields = ["created_at"]
-    list_display = ["address", "price", "new_building", "construction_year", "town"]
-    list_editable = ["new_building"]
-    list_filter = ["new_building", "town", "construction_year"]
-    raw_id_fields = ["likes"]
-
-class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ["complaint"]
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Pokemon)
+admin.site.register(PokemonEntity)
